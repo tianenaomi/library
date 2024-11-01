2fb16a43471c76e536f784e5bdfc6157a7a4faa5
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2024-11-01 13:02:13 +1000

    ADD logic to display book cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const addBtn = document.getElementById('addBtn');
+const addBook = document.getElementById('addBook');
 const shelf = document.getElementById('shelf');
 const myLibrary = [
     {   Title: "Pride and Prejudice",
@@ -34,14 +34,76 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
-    
+    // 
 }
 
 function displayBooks(myLibrary){
-    // FOR each book in myLibrary
-        // CREATE card (div)
-        // ADD book details to card
-        // ADD button to card
-        // ADD class to div
-        // APPEND card to shelf (div)
+    for (let i = 0; i < myLibrary.length; i++){
+        let card = document.createElement('div');
+        let title = document.createElement('p');
+        title.textContent = `Title: ${myLibrary[i].Title}`;
+        let author = document.createElement('p');
+        author.textContent = `Author: ${myLibrary[i].Author}`;
+        let pages = document.createElement('p');
+        pages.textContent = `Pages: ${myLibrary[i].Pages}`;
+        let read = document.createElement('p');
+        read.textContent = `Read: ${myLibrary[i].Read}`;
+        let button = document.createElement('button');
+        button.textContent = 'remove';
+        card.appendChild(title)
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+        card.appendChild(button);
+        card.classList.add('card');
+        shelf.appendChild(card);
+    } 
 }
+
+displayBooks(myLibrary);
+
+
+/*
+PAGE loads
+DISPLAYS current collection
+CLICK add book
+    OUTPUT form
+    SUBMIT data
+    CREATES book
+    ADD book to collection
+
+DISPLAY BOOKS - RESULTS OF ATTEMPT 1
+    - creates an array with 5(?) items(div.card)
+    - each array has all books therefore books are multiplied by 5
+    - TypeError at return statement Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
+
+    let book = [];
+    for (let i = 0; i <= myLibrary.length; i++){
+        // CREATE card (div)
+        book[i] = document.createElement('div');
+        //FOR EACH PROPERTY
+        for (const [key, value] of Object.entries(myLibrary)){
+            //CREATE DOM element
+            let property = document.createElement('div');
+            //output property to element
+            property.textContent = `${value}`;
+            //append element to book[i]
+            book[i].appendChild(property);
+        //END FOR    
+        }
+        // CREATE delete button 
+        let button = document.createElement('button');
+        // ADD text to button
+        button.textContent = 'remove';
+        // ADD button to card
+        book[i].appendChild(button);
+        // ADD class to div
+        book[i].classList.add('card');
+    // END FOR    
+    }
+    // APPEND card to shelf (div)
+    return shelf.appendChild(book);
+}    
+
+
+*/
